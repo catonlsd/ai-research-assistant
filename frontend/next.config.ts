c96c1bf3f92e9b55e9d2a7e8eab822e596e709b3
Mokshit <paranoidjalebi@gmail.com>
2026-05-12 15:54:00 +0530

    updated frontend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true
+  async rewrites() {
+    return [
+      {
+        source: "/backend/:path*",
+        destination:
+          "https://ai-research-assistant-backend-eoew.onrender.com/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
